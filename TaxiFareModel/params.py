c2d76741a8cafe16bb691cd5282e643595f33518
Matheus Oliveira <matheus_so@yahoo.com.br>
2022-03-29 20:38:10 -0300

    updated params.py

--- TaxiFareModel/params.py
+++ TaxiFareModel/params.py
@@ -1,7 +1,7 @@
 ### MLFLOW configuration - - - - - - - - - - - - - - - - - - -
 
 MLFLOW_URI = "https://mlflow.lewagon.co/"
-EXPERIMENT_NAME = "[BR] [BH] [matheussposito] TFM_predict_in_production + 2"
+EXPERIMENT_NAME = "[BR] [BH] [matheussposito] TFM_predict_in_production2"
 
 ### DATA & MODEL LOCATIONS  - - - - - - - - - - - - - - - - - - -
 
